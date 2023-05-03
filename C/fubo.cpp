4178fb6eb659f2cbfbc475cf24ec656f51020f2e
Preksha1311 <108273216+Preksha1311@users.noreply.github.com>
2023-05-03 22:15:36 +0530

    one more

---
+++ C/fubo.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int fib (int n){
+      //base 
+if (n<=1)
+    return n;
+
+    cout<<"hi";
+    return (fib(n-1) + fib(n-2));
+}
+
+int main(){
+    
+    //fibonacci
+    
+    int n;
+    cout<<"enter the terms"<<endl;
+    cin>>n;
+
+    cout<<fib(n);
+
+    return 0;
+}
+// 0 1 1 2 3 5 8 13 21 34 
+

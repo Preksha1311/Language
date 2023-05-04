0e2071e6cd139293a0e6d9be0b154f2deaad3543
Preksha1311 <108273216+Preksha1311@users.noreply.github.com>
2023-05-04 20:03:23 +0530

    pointer

--- C/pointer.cpp
+++ C/pointer.cpp
@@ -4,9 +4,11 @@ int main()
 {
     int a = 90;
     int *point = &a;
+    int **poo = &point;
     cout << *point << endl;
     cout << a << endl;
     cout << point << endl;
+    cout <<"double dereferencing of a "<< **poo << endl;
     cout << &a << endl;
     return 0;
 }
